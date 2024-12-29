605f0f8679d204c609407989467c489f9b9d694d
kd pc <dongkp96@gmail.com>
2024-12-29 15:47:19 -0500

    Added HTML and JS

---
+++ TOP-Calculator-main/index.js
@@ -0,0 +1,26 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a, b){
+    if(b===0){
+        return "Error, cannot divide by 0";
+    }else{
+        return a/b;
+    }
+}
+
+function inputNumber(event){
+    let number = document.querySelector("numbers-bar").innerHTML;
+    switch(event){
+        case
+    }
+}
